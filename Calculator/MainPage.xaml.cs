54e9017ad71f612807e3110dd489d7790b1cd343
derphead <spalderpank@gmail.com>
2013-09-07 15:20:16 +0200

    I don't even know what I'm doing with this one

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            TextBlock textBlock2 = new TextBlock();
+            textBlock2.Name = "textBlock";
+            textBlock2.Height = 23;
+            textBlock2.HorizontalAlignment = HorizontalAlignment.Right;
+            textBlock2.Margin = new Thickness(0, 34, 70, 0);
+            textBlock2.Text = "Hello from the .cs file!";
+            
+            layoutGrid.Children.Add(textBlock2);
+            Grid.SetRow(textBlock2, 1);
+            Grid.SetColumn(textBlock2, 2);
+        }
+    }
+}
